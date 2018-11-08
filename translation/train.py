12f903fafe4e6109ffc8e1f94d9b7e3e17fd0225
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-11-08 00:13:36 -0800

    Added initial training files

--- translation/train.py
+++ translation/train.py
@@ -1,1 +1,21 @@
 import torch
+import vocab
+import constants 
+import conll2003dataloader
+import pickle
+import argparse
+import os
+import models.bilstm_crf as bilstm_crf
+import torch
+from train_args import get_arg_parser
+from tensor_logger import Logger
+
+from tqdm import trange, tqdm
+
+def main(args):
+    pass
+
+if __name__ == "__main__":
+    parser = get_arg_parser()
+    args = parser.parse_args()
+    main(args)
---
+++ translation/train_args.py
@@ -0,0 +1,32 @@
+import argparse
+import time
+
+def get_arg_parser():
+    '''
+    Create arg parse for training containing options for 
+        optimizer
+        hyper parameters
+        model saving
+        log directories
+    '''
+    parser = argparse.ArgumentParser(description='Train Named Entity Recognition on Train Conll2003.')
+
+    # Parser data loader options
+    parser.add_argument('--load', action='store_true', help='load the saved files')
+    parser.add_argument('--save', action='store_true', help='save the loaded data loaders')
+    parser.add_argument('--clean', action='store_true', help='delete saved files')
+    parser.add_argument('--train_bi_lstm', action='store_true', help='train the BiLSTM CRF model')
+    parser.add_argument('--num_epochs', type=int, default=2, help='number of epochs to train on')
+    parser.add_argument('--num_workers', type=int, default=2, help='number of workers for dataset')
+    parser.add_argument('--shuffle', type=bool, default=True, help='should shuffle the dataset for training')
+    parser.add_argument('--batch_size', type=int, default=64, help='the batch size for specified training')
+    parser.add_argument('--embedding_dim', type=int, default=300, help='embedding dimensions for the LSTM features')
+    parser.add_argument('--hidden_dim', type=int, default=1000, help='hidden dimensions of LSTM outputs for feature size')
+    parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate for optimizer')
+    parser.add_argument('--weight_decay', type=float, default=1e-4, help='weight decay for weight update')
+    parser.add_argument('--save_dir', type=str, default='saved_models/', help='directory for saved models')
+    parser.add_argument('--model_name', type=str, default='model_{}'.format(time.time()), help='model_name')
+    parser.add_argument('--log_dir', type=str, default='tensor_logs/', help='model_name')
+    parser.add_argument('--model_path', type=str, help='the path for the saved model')
+
+    return parser
