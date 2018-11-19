20504559f5e7b9bbf149ce1b19052a5203fe13f0
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-11-19 01:05:12 -0800

    updated train for torchtext testing

--- translation/train.py
+++ translation/train.py
@@ -1,19 +1,40 @@
 import torch
-import vocab
-import constants 
-import conll2003dataloader
 import pickle
 import argparse
 import os
-import models.bilstm_crf as bilstm_crf
-import torch
-from train_args import get_arg_parser
-from tensor_logger import Logger
-
 from tqdm import trange, tqdm
+import torch
+import torchtext
+from torchtext import data
+from torchtext import datasets
+
+from train_args import get_arg_parser
+import constants
+
+
+def train():
+    src = data.Field()
+    trg = data.Field()
+    mt_train = datasets.TranslationDataset(
+        path=constants.WMT14_EN_FR_VALID, exts=('.en', '.fr'),
+        fields=(src, trg),
+    )
+
+    src.build_vocab(mt_train, max_size=80000)
+    trg.build_vocab(mt_train, max_size=40000)
+
+    train_iter = data.BucketIterator(
+        dataset=mt_train,
+        batch_size=32,
+        sort_key=lambda x: data.interleave_keys(len(x.src), len(x.trg))
+    )
+
+    a = (next(iter(train_iter)))
+    print(a)
+    print('done')
 
 def main(args):
-    pass
+    train()
 
 if __name__ == "__main__":
     parser = get_arg_parser()
