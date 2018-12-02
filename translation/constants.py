c2b04829a52cb959a34f776524ae77be9bd30d6c
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-12-02 02:40:54 -0800

    added constants for translation

--- translation/constants.py
+++ translation/constants.py
@@ -22,4 +22,4 @@ SMALL_TRAIN_FR_VOCAB_FILE = os.path.join(WMT14_EN_FR_DIR, 'small_train_fr_vocab.
 
 TORCH_TEXT_DATA_SET_FILE = os.path.join(WMT14_EN_FR_DIR, 'tt_small_train_dataset.pkl')
 TORCH_TEXT_SMALL_EN_VOCAB_FILE = os.path.join(WMT14_EN_FR_DIR, 'tt_small_train_en_vocab.pkl')
-TORCH_TEXT_SMALL_TRAIN_FR_VOCAB_FILE = os.path.join(WMT14_EN_FR_DIR, 'tt_small_train_fr_vocab.pkl')
+TORCH_TEXT_SMALL_FR_VOCAB_FILE = os.path.join(WMT14_EN_FR_DIR, 'tt_small_train_fr_vocab.pkl')
