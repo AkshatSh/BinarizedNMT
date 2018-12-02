5d4dd4b883840e979509018e2f846ab50150976a
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-12-02 02:40:37 -0800

    added loading vocab rather than computing

--- preprocess_torchtext.py
+++ preprocess_torchtext.py
@@ -14,6 +14,7 @@ import math
 from typing import Tuple
 
 from translation import constants
+from translation import train_args
 
 # dataset, src_field, trg_field
 LoadedDatasetType = Tuple[datasets.TranslationDataset, data.Field, data.Field]
@@ -21,11 +22,21 @@ LoadedDatasetType = Tuple[datasets.TranslationDataset, data.Field, data.Field]
 # src_vocab, trg_vocab
 TranslationVocabularyType = Tuple[Vocab, Vocab]
 
-def load_wmt_small_dataset() -> LoadedDatasetType:
-    src = data.Field(include_lengths=True,
-               init_token='<sos>', eos_token='<eos>', batch_first=True, fix_length=200)
-    trg = data.Field(include_lengths=True,
-               init_token='<sos>', eos_token='<eos>', batch_first=True)
+def load_wmt_small_dataset(args: argparse.ArgumentParser) -> LoadedDatasetType:
+    src = data.Field(
+        include_lengths=True,
+        init_token='<sos>',
+        eos_token='<eos>',
+        batch_first=True,
+        fix_length=args.torchtext_src_fix_length,
+    )
+
+    trg = data.Field(
+        include_lengths=True,
+        init_token='<sos>',
+        eos_token='<eos>',
+        batch_first=True,
+    )
 
     mt_train = datasets.TranslationDataset(
         path=constants.WMT14_EN_FR_SMALL_TRAIN,
@@ -36,20 +47,32 @@ def load_wmt_small_dataset() -> LoadedDatasetType:
     return mt_train, src, trg
 
 def load_vocabulary(
+    args: argparse.ArgumentParser,
     dataset_info: LoadedDatasetType,
 ) -> TranslationVocabularyType:
     mt_train, src, trg = dataset_info
-    src.build_vocab(mt_train, min_freq=2, max_size=80000)
-    trg.build_vocab(mt_train, max_size=10000)
+    src.build_vocab(
+        mt_train,
+        min_freq=args.torchtext_unk,
+        max_size=args.torchtext_src_max_vocab,
+    )
+
+    trg.build_vocab(
+        mt_train,
+        max_size=args.torchtext_trg_max_vocab,
+    )
     return src.vocab, trg.vocab
 
 def main() -> None:
+    parser = train_args.get_arg_parser()
+    args = parser.parse_args()
+
     print('[INFO]: loading dataset ...')
-    dataset_info = load_wmt_small_dataset()
+    dataset_info = load_wmt_small_dataset(args)
     print('[INFO]: loaded dataset')
 
     print('[INFO]: building vocabulary ...')
-    src_vocab, trg_vocab = load_vocabulary(dataset_info)
+    src_vocab, trg_vocab = load_vocabulary(args, dataset_info)
     print('[INFO]: built vocabulary')
 
     print('[INFO]: saving vocabulary ...')
@@ -64,4 +87,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
