f17aca73f410a1cee2c297277ce0d179ed709d81
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-12-02 23:43:54 -0800

    updated convseq2seq imports

--- translation/models/ConvSeq2Seq.py
+++ translation/models/ConvSeq2Seq.py
@@ -1,4 +1,29 @@
+import sys
+sys.path.append("..")
+
 import torch
+from torch import nn
+import torch.nn.functional as F
+import random
+import argparse
+
+from models.EncoderDecoder import (
+    EncoderModel,
+    DecoderModel,
+    EncoderDecoderModel,
+    DecoderOutputType,
+)
+
+from models.components.attention import (
+    AttentionModule
+)
+
+from vocab import Vocabulary
+
+from constants import (
+    UNKNOWN_TOKEN,
+    PAD_TOKEN,
+)
 
 '''
 Implementation of the convolutional sequence to sequence architecture described here:
