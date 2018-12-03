184613d97c6c626f3483924e55f0f03b75bdfb6d
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-12-02 23:39:21 -0800

    initial commit for convseq2seq

---
+++ translation/models/ConvSeq2Seq.py
@@ -0,0 +1,9 @@
+import torch
+
+'''
+Implementation of the convolutional sequence to sequence architecture described here:
+https://arxiv.org/pdf/1705.03122.pdf 
+
+
+Adopted from: Fair Seq https://github.com/pytorch/fairseq/blob/master/fairseq/models/fconv.py
+'''
