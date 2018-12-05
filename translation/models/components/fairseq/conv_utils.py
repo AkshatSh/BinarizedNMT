7fe17d7a717ca50c06918c9300f1622611cdb1cd
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-12-04 17:59:34 -0800

    added a projection layer to replace linear layers in encoder

--- translation/models/components/fairseq/conv_utils.py
+++ translation/models/components/fairseq/conv_utils.py
@@ -11,12 +11,16 @@ from .linearized_conv import LinearizedConvolution as fairseq_linear_conv
 
 from .conv_tbc import ConvTBC as fairseq_convtbc
 
+USE_SIMPLE = True
+
 def Embedding(
     num_embeddings: int, 
     embedding_dim: int,
     padding_idx: int,
 ) -> nn.Module:
     m = nn.Embedding(num_embeddings, embedding_dim, padding_idx=padding_idx)
+    if USE_SIMPLE:
+        return m
     nn.init.normal_(m.weight, 0, 0.1)
     nn.init.constant_(m.weight[padding_idx], 0)
     return m
@@ -28,6 +32,8 @@ def PositionalEmbedding(
 ) -> nn.Module:
     # left_pad = False, since everything is right padded here
     m = LearnedPositionalEmbedding(num_embeddings, embedding_dim, padding_idx, False)
+    if USE_SIMPLE:
+        return m
     nn.init.normal_(m.weight, 0, 0.1)
     nn.init.constant_(m.weight[padding_idx], 0)
     return m
@@ -39,6 +45,8 @@ def Linear(
 ) -> nn.Module:
     """Weight-normalized Linear layer (input: N x T x C)"""
     m = nn.Linear(in_features, out_features)
+    if USE_SIMPLE:
+        return m
     nn.init.normal_(m.weight, mean=0, std=math.sqrt((1 - dropout) / in_features))
     nn.init.constant_(m.bias, 0)
     return nn.utils.weight_norm(m)
@@ -71,3 +79,22 @@ def ConvTBC(
     nn.init.normal_(m.weight, mean=0, std=std)
     nn.init.constant_(m.bias, 0)
     return nn.utils.weight_norm(m, dim=2)
+
+def Conv1d(in_channels, out_channels, kernel_size, dropout=0, **kwargs):
+    """Weight-normalized Conv1d layer"""
+    m = nn.Conv1d(in_channels, out_channels, kernel_size, **kwargs)
+    if USE_SIMPLE:
+        return m
+    std = math.sqrt((4 * (1.0 - dropout)) / (m.kernel_size[0] * in_channels))
+    m.weight.data.normal_(mean=0, std=std)
+    m.bias.data.zero_()
+    return nn.utils.weight_norm(m)
+
+def Projection(in_features, out_features, dropout=0):
+    """Weight-normalized Linear via 1x1 convolution (input: N x C x T)"""
+    m = nn.Conv1d(in_features, out_features, kernel_size=1)
+    if USE_SIMPLE:
+        return m
+    m.weight.data.normal_(mean=0, std=math.sqrt((1 - dropout) / in_features))
+    m.bias.data.zero_()
+    return nn.utils.weight_norm(m)
