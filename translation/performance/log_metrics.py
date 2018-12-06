7b38776174d626066990ff09614bc60e494d3123
Kevin Bi <kevin_bi@outlook.com>
2018-12-05 17:57:24 -0800

    logging code is ready to be tested

--- translation/performance/log_metrics.py
+++ translation/performance/log_metrics.py
@@ -4,6 +4,7 @@ import time
 import string
 import sys
 import subprocess
+import re
 from tensor_logger import Logger
 
 """
@@ -15,7 +16,7 @@ GPU_MEM = "Used GPU Memory"
 
 def get_cpumem(pid):
     try:
-        process = subprocess.Popen(['ps', 'ux'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        process = subprocess.Popen(['ps', 'ux'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     except e:
         return None
     out, err = process.communicate()
@@ -24,31 +25,31 @@ def get_cpumem(pid):
     out = out.decode("utf-8")
     d = [i for i in out.split('\n') if len(i) > 0 and i.split()[1] == str(pid)]
     if d:
-        return (float(d[0].split()[2]), float(d[0].split()[3]), int(d[0].split()[5]))
+        # (CPU%, MEM%, MEM)
+        # MEM is in term of Kilobytes (2^10)
+        return (float(d[0].split()[2]), float(d[0].split()[3]), int(d[0].split()[5]) * 1000)
     else:
         return None
 
 def get_gpumem(pid):
     try:
-        process = subprocess.Popen(['nvidia-smi', '-q'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        process = subprocess.Popen(['gpustat', '-cp'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     except:
         return None
     out, err = process.communicate()
     if out is None:
         return None
     out = out.decode("utf-8")
-    out_dict = {}
+    pid_tag = "python/" + str(pid)
+    gpu_per, gpu = None, None
     for item in out.split('\n'):
-        try:
-            key, val = item.split(':')
-            key, val = key.strip(), val.strip()
-            out_dict[key] = val
-        except:
-            pass
-    x = out_dict[GPU_PER].replace(' ', '')
-    x = float(x.replace('%', ''))
-    y = out_dict[GPU_MEM].replace(' ', '')  # need to convert this from whatever suffix to bytes
-    return (x, y) 
+        if pid_tag in item:
+            try:
+                gpu_per = [int(s) for s in item.split("|")[1].split(",")[1].split() if s.isdigit()][0]
+                gpu = (re.search(r'\((.*?)\)', item).group(1))
+            except:
+                pass
+    return (gpu_per, gpu*(2**20)) # GPU memory is in term of Mibibytes (2^20)
 
 def log(pid, poll_period=1800, log_dir=None):
     logger = None
@@ -58,21 +59,23 @@ def log(pid, poll_period=1800, log_dir=None):
         logger = Logger(log_dir)
     try:
         while True:
+            # Get CPU and MEM stats
             cpu_res = get_cpumem(pid)
             if cpu_res is None: 
-                cpu_per,mem_per,mem = None, None, None
+                cpu_per,mem_per,mem = 0, 0, 0
             else:
                 cpu_per,mem_per,mem = cpu_res
-
+            # Get GPU stats
             gpu_res =  get_gpumem(pid)
             if gpu_res is None:
-                gpu_per,gpu = None, None
+                gpu_per,gpu = 0, 0
             else:
                 gpu_per,gpu = gpu_res
-
+            # Exit if nothing is gotten
             if gpu_res is None and cpu_res is None:
                 exit(1)
-
+                
+            # log stats
             if logger is None:
                print("{},{},{},{},{}".format(cpu_per,mem_per,mem,gpu_per,gpu))
             else:
